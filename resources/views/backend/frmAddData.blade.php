3f198b16a322dfdd5dddd19978dc49b09f5c317a
dalorkee <dalorkee@gmail.com>
2018-02-28 16:12:37 +0700

    gen form

--- resources/views/backend/frmAddData.blade.php
+++ resources/views/backend/frmAddData.blade.php
@@ -14,44 +14,51 @@
 		</section>
 		<!-- Main content -->
 		<section class="content">
-			<div class="container-fluid">
-				<form>
-					<fieldset>
-						<legend>ข้อมูลบุคคล</legend>
-						<div class="row">
-							<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2">
-								<div class="form-group">
-									<label for="index_number">ลำดับที่</label>
-									<input type="text" class="form-control" placeholder="ลำดับที่">
-								</div>
-							</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="box box-primary">
+						<div class="box-header with-border">
+							<h3 class="box-title">ข้อมูลบุคคล</h3>
 						</div>
-						<div class="row">
-							<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-2">
+						<form role="form">
+							<div class="box-body">
+								<div class="row">
+									<div class="col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl-2">
+										<div class="form-group">
+											<label for="exampleInputEmail1">ลำดับที่</label>
+											<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-2">
+										<div class="form-group">
+											<label for="service_type">ประเภทผู้ขอรับบริการ</label>
+											<select name="service_type" class="form-control">
+												<option value="1">รัฐบาล</option>
+												<option value="2">เอกชน</option>
+												<option value="100">อื่นๆ</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+										<div class="form-group">
+											<label for="service_type_etc">ประเภทผู้ขอรับบริการอื่นๆุ</label>
+											<input type="text" class="form-control" placeholder="ประเภทผู้ขอรับบริการอื่นๆ โปรดระบุ">
+										</div>
+									</div>
+								</div>
 								<div class="form-group">
-									<label for="service_type">ประเภทผู้ขอรับบริการ</label>
-									<select name="service_type" class="form-control">
-										<option value="1">รัฐบาล</option>
-										<option value="2">เอกชน</option>
-										<option value="100">อื่นๆ</option>
-									</select>
+									<label for="exampleInputPassword1">Password</label>
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 								</div>
 							</div>
-							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-								<div class="form-group">
-									<label for="service_type_etc">ประเภทผู้ขอรับบริการอื่นๆุ</label>
-									<input type="text" class="form-control" placeholder="ประเภทผู้ขอรับบริการอื่นๆ โปรดระบุ">
-								</div>
+							<div class="box-footer">
+								<button type="submit" class="btn btn-primary">Submit</button>
 							</div>
-						</div>
-						<div class="row">
-							<div class="input-append date" id="datetimepicker" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
-						        <input class="span2" size="16" type="text" value="12-02-2012">
-						        <span class="add-on"><i class="icon-remove"></i></span>
-						        <span class="add-on"><i class="icon-th"></i></span>
-						    </div>            
-					</fieldset>
-				</form>
+						</form>
+					</div>
+				</div>
 			</div>
 		</section>
 		<!-- /.content -->
