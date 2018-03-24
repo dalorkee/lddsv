bc293cb81caa45f9bc6853db9f63be114be0a747
dalorkee <dalorkee@gmail.com>
2018-03-24 16:03:15 +0700

    create modal

--- resources/views/backend/frmAddData.blade.php
+++ resources/views/backend/frmAddData.blade.php
@@ -77,6 +77,14 @@
 											</div>
 										</div>
 									</div>
+									<div class="row">
+										<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+											<div class="form-group">
+												<label>หมายเลขทะเบียนนิติบุคคล (ถ้ามี)</label>
+												<input type="text" name="cust_company_idx" class="form-control" placeholder="หมายเลขทะเบียนนิติบุคคล">
+											</div>
+										</div>
+									</div>
 								</div>
 								<div id="personal">
 									<div class="row">
@@ -204,6 +212,11 @@
 							<fieldset>
 								<legend>รายการข้อมูล</legend>
 								<div class="row">
+									<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-2">
+										<div class="form-group">
+											<button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-plus-circle"></i> Add Data</button>
+										</div>
+									</div>
 								</div>
 							</fieldset>
 							<fieldset>
@@ -364,6 +377,47 @@
 			</div>
 		</div>
 	</section>
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel" style="display: inline-block;">Modal title</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+							<div class="form-group">
+								<label for="data">ข้อมูล</label>
+								<select name="service_data" class="form-control select2" style="width: 100%">
+									<option value="0">- โปรดเลือก -</option>
+									<option value="1">ภาพถ่ายทางอากาศสีเชิงเลข</option>
+									<option value="2">แผนที่ภาพถ่ายออร์โธสี</option>
+								</select>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+							<div class="form-group">
+								<label for="data_resolution">ความละเอียด</label>
+								<select name="service_data_resolution" class="form-control select2" style="width: 100%">
+									<option value="0">- โปรดเลือก -</option>
+									<option value="1">1:25,000</option>
+									<option value="2">1:4,000</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- /.content-wrapper -->
 @stop
